2f1c4c884ed68b653799386553d5bf622a24d938
Silvio Böhler <sboehler@noreply.users.github.com>
2022-12-03 16:52:30 +0100

    Day 3 part 2

---
+++ src/day3/day3.rs
@@ -0,0 +1,44 @@
+use std::{
+    collections::HashSet,
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::Path,
+};
+
+pub fn compute(p: &Path) -> io::Result<i64> {
+    let f = File::open(p)?;
+    let mut lines = BufReader::new(f).lines();
+
+    let mut res = 0;
+    while let Some(line) = lines.next()? {
+        let mut s = HashSet::from_iter(line.chars());
+        for _ in 0..2 {
+            let line = lines
+                .next()
+                .ok_or(io::Error::new(io::ErrorKind::UnexpectedEof, ""))??;
+            let chars = line.chars().collect::<HashSet<_>>();
+            s.retain(|c| chars.contains(c))
+        }
+        s.iter().for_each(|c| res += score(*c));
+    }
+    Ok(res)
+}
+
+fn score(c: char) -> i64 {
+    let v = c as i64;
+    match v {
+        65..=90 => v - 38,
+        97..=122 => v - 96,
+        _ => 0,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_compute() {
+        assert_eq!(compute(Path::new("src/day3/example.txt")).unwrap(), 70);
+    }
+}
