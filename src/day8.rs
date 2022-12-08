c1f50059a612ad4d3421439939bdec6c7e5ce7da
Silvio Böhler <sboehler@noreply.users.github.com>
2022-12-09 00:49:29 +0100

    More cleanups

--- src/day8.rs
+++ src/day8.rs
@@ -10,86 +10,120 @@ pub enum Part {
 
 pub fn compute(p: &Path, part: Part) -> Result<usize> {
     let s = fs::read_to_string(p)?;
-    let width = s.bytes().position(|b| b == b'\n').unwrap_or(s.len());
-    let trees = s
-        .chars()
-        .filter(|ch| *ch != '\n')
-        .map(|ch| ch as isize - 48)
-        .collect::<Vec<_>>();
+    let matrix = Matrix {
+        width: s.bytes().position(|b| b == b'\n').unwrap_or(s.len()),
+        values: s
+            .chars()
+            .filter(|ch| *ch != '\n')
+            .map(|ch| ch as isize - 48)
+            .collect::<Vec<_>>(),
+    };
     match part {
-        Part::One => Ok(part1(&trees, width)),
-        Part::Two => Ok(part2(&trees, width)),
+        Part::One => Ok(part1(&matrix)),
+        Part::Two => Ok(part2(&matrix)),
     }
 }
 
-fn part1(trees: &[isize], width: usize) -> usize {
-    let len = trees.len();
-    let height = trees.len() / width;
-    let mut visible = vec![false; trees.len()];
+fn part1(matrix: &Matrix<isize>) -> usize {
+    let width = matrix.width;
+    let height = matrix.height();
+    let mut visible = vec![false; matrix.values.len()];
 
-    for line_idx in (0..len).step_by(height) {
-        fill_visibility(trees, &mut visible, line_idx..line_idx + width);
-        fill_visibility(trees, &mut visible, (line_idx..line_idx + width).rev())
+    for row in 0..height {
+        fill_visibility(matrix, &mut visible, matrix.row(row, 0, width));
+        fill_visibility(matrix, &mut visible, matrix.row(row, 0, width).rev());
     }
-    for col_idx in 0..width {
-        fill_visibility(trees, &mut visible, (col_idx..len).step_by(height));
-        fill_visibility(trees, &mut visible, (col_idx..len).step_by(height).rev());
+    for col in 0..width {
+        fill_visibility(matrix, &mut visible, matrix.col(col, 0, height));
+        fill_visibility(matrix, &mut visible, matrix.col(col, 0, height).rev());
     }
     visible.iter().filter(|t| **t).count()
 }
 
-fn fill_visibility<I>(heights: &[isize], visible: &mut [bool], iter: I)
+fn fill_visibility<I>(matrix: &Matrix<isize>, visible: &mut [bool], iter: I)
 where
     I: Iterator<Item = usize>,
 {
     iter.fold(isize::MIN, |max, i| {
-        if heights[i] > max {
+        if matrix.values[i] > max {
             visible[i] = true;
-            heights[i]
+            matrix.values[i]
         } else {
             max
         }
     });
 }
 
-fn part2(trees: &[isize], width: usize) -> usize {
-    let height = trees.len() / width;
-    let mut max = 0;
-    for line in 1..height - 1 {
-        let line_offset = line * height;
-        for col in 1..width - 1 {
-            let idx = line * height + col;
-            let h = trees[idx];
-            let right = (line_offset + col + 1..line_offset + width)
-                .position(|i| trees[i] >= h)
-                .map(|i| i + 1)
-                .unwrap_or(width - col - 1);
+fn part2(matrix: &Matrix<isize>) -> usize {
+    let width = matrix.width;
+    let height = matrix.height();
 
-            let left = (line_offset..line_offset + col)
-                .rev()
-                .position(|i| trees[i] >= h)
-                .map(|i| i + 1)
-                .unwrap_or(col);
+    (1..height - 1)
+        .flat_map(|row| {
+            (1..width - 1).map(move |col| {
+                let idx = row * height + col;
+                let h = matrix.values[idx];
+                let right = matrix
+                    .row(row, col + 1, width)
+                    .position(|i| matrix.values[i] >= h)
+                    .map(|i| i + 1)
+                    .unwrap_or(width - col - 1);
 
-            let bottom = (idx + height..width * height)
-                .step_by(height)
-                .position(|i| trees[i] >= h)
-                .map(|i| i + 1)
-                .unwrap_or(height - line - 1);
+                let left = matrix
+                    .row(row, 0, col)
+                    .rev()
+                    .position(|i| matrix.values[i] >= h)
+                    .map(|i| i + 1)
+                    .unwrap_or(col);
 
-            let top = (col..idx)
-                .step_by(height)
-                .rev()
-                .position(|i| trees[i] >= h)
-                .map(|i| i + 1)
-                .unwrap_or(line);
-            let score = right * left * top * bottom;
-            if score > max {
-                max = score
-            }
-        }
+                let bottom = matrix
+                    .col(col, row + 1, height)
+                    .position(|i| matrix.values[i] >= h)
+                    .map(|i| i + 1)
+                    .unwrap_or(height - row - 1);
+
+                let top = matrix
+                    .col(col, 0, row)
+                    .rev()
+                    .position(|i| matrix.values[i] >= h)
+                    .map(|i| i + 1)
+                    .unwrap_or(row);
+                right * left * top * bottom
+            })
+        })
+        .max()
+        .unwrap_or(usize::MIN)
+}
+
+struct Matrix<T> {
+    pub values: Vec<T>,
+    pub width: usize,
+}
+
+impl<T> Matrix<T> {
+    fn height(&self) -> usize {
+        return self.values.len() / self.width;
     }
-    max
+
+    fn col(
+        &self,
+        col: usize,
+        start: usize,
+        end: usize,
+    ) -> impl DoubleEndedIterator<Item = usize> + ExactSizeIterator {
+        let height = self.values.len() / self.width;
+        (col..end * height).step_by(height).skip(start)
+    }
+
+    fn row(
+        &self,
+        row: usize,
+        start: usize,
+        end: usize,
+    ) -> impl DoubleEndedIterator<Item = usize> + ExactSizeIterator {
+        let height = self.values.len() / self.width;
+        row * height + start..row * height + end
+    }
 }
 
 #[cfg(test)]
